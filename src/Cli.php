b5944eacdebe4f88b5e26dc51bae8a5801134c33
MikkyBloom <you@example.com>
2022-09-23 17:18:20 +0300

    added linter

--- src/Cli.php
+++ src/Cli.php
@@ -5,9 +5,9 @@ namespace BrainGames\Cli;
 use function cli\line;
 use function cli\prompt;
 
-function welcome($name) {
+function welcome($name)
+{
     line('Welcome to the Brain Game!');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
 }
-
