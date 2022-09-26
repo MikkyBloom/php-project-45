dd50071e767d7fc3103d589924ca4d11bdfa00fa
MikkyBloom <you@example.com>
2022-09-26 20:11:32 +0300

    made brain-even

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/MikkyBloom/php-project-45/workflows/hexlet-check/badge.svg)](https://github.com/MikkyBloom/php-project-45/actions)
 <a href="https://codeclimate.com/github/MikkyBloom/php-project-45/maintainability"><img src="https://api.codeclimate.com/v1/badges/642b4cee01dca5652890/maintainability" /></a>
+### asciinema brain-even:
+https://asciinema.org/a/vtM45Zwb3gk9Mjnz89AAqtyOX
--- src/Engine.php
+++ src/Engine.php
@@ -7,8 +7,6 @@ use function cli\prompt;
 
 function isEven($name)
 {
-    // $nums = [17, 23, 19];
-    // $answers = ['yes', 'yes', 'no'];
     $numbers = ['15' => 'no', '6' => 'yes', '7' => 'no'];
 
     line('Answer "yes" if the number is even, otherwise answer "no".');
@@ -16,8 +14,8 @@ function isEven($name)
     foreach ($numbers as $key => $value) {
         $answer = prompt("Question: $key\nYour answer");
         if ($answer !== $value) {
-    	    print_r("'$answer' is wrong answer ;(. Correct answer was '$value'. \nLet's try again, $name\n");
-    	    exit;
+            print_r("'$answer' is wrong answer ;(. Correct answer was '$value'. \nLet's try again, $name\n");
+            exit;
         } else {
             print_r("Correct!\n");
         }
