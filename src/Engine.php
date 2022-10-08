be71a46fcf54c563e31c4efff746f902ff11fdf8
MikkyBloom <you@example.com>
2022-10-08 19:05:42 +0300

    .

--- src/Engine.php
+++ src/Engine.php
@@ -137,7 +137,7 @@ function isPrime(string $name)
 
     foreach ($randArray as $number) {
         $questions[] = $number;
-        if (in_array($number, $primeNumbers)) {
+        if (in_array($number, $primeNumbers, bool $strict = false)) {
             $answers[] = 'yes';
             continue;
         }
